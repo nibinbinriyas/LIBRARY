8e7fa9ae5e8bf1dec5a23bda779a1fe94b86481d
nibinbinriyas <nibinbinriyas@gmail.com>
2021-09-28 00:38:41 +0530

    modified

--- library/forms.py
+++ library/forms.py
@@ -3,14 +3,14 @@ from wtforms import StringField,IntegerField,SubmitField
 
 class AddForm(FlaskForm):
 
-    name = StringField("Title of Book : ")
-    auther = StringField("Auther of Book : ")
+    name = StringField("Title of Book    ")
+    auther = StringField("Auther of Book    ")
     submit = SubmitField("Add Book")
 
 class DelForm(FlaskForm):
-    id = IntegerField("Id of the Book")
+    id = IntegerField("Id of the Book    ")
     submit = SubmitField("Buy")
 
 class SearchForm(FlaskForm):
-    auther = StringField("Enter Auther's Name : ")
+    auther = StringField("Enter Auther's Name    ")
     submit = SubmitField("Search")
