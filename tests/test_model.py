e2c4a832afe10a1779c4ff1bc9ab31fd1e78b17f
nibinbinriyas <nibinbinriyas@gmail.com>
2021-09-28 23:26:59 +0530

    requirement_dev.txt added

--- tests/test_model.py
+++ tests/test_model.py
@@ -1,12 +1,11 @@
 from library.models import Book
 from library import db
-#put in function
 
 def test_Book():
     newbook = Book('Half girl friend','Chetan Bagat')
 
     db.session.add(newbook)
-    x = db.session.commit()
+    db.session.commit()
 
     books = Book.query.all()
     for book in books:
